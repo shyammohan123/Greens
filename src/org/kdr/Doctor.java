a360bdeee69af283eb3b89fa77a856afa9975576
shyammohan123 <shyammohan123@gmail.com>
2022-11-21 11:08:26 +0530

    Login Functionality Louis

--- src/org/kdr/Doctor.java
+++ src/org/kdr/Doctor.java
@@ -6,3 +6,17 @@ public class Doctor {
 		}
 
 }
+private void test5() {
+	System.out.println("5");
+
+}
+private void test6() {
+	System.out.println("6");
+
+}
+private void test7() {
+	System.out.println("7");
+
+}
+private void test8() {
+	System.out.println("8");
